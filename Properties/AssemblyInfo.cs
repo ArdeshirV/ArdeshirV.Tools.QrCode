230385662ffcca9b7bbbefa46a765ed11663b6ab
ArdeshirV <ardeshirv@protonmail.com>
2020-01-02 15:05:03 +0330

    Add files

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,47 @@
+/* 
+ * QR code generator library (.NET)
+ *
+ * Copyright (c) ArdeshirV@protonmail.com (MIT License)
+ * https://ardeshirv.github.io/ArdeshirV.Utility.QrCode/
+ * Copyright (c) Manuel Bleichenbacher (MIT License)
+ * https://github.com/manuelbl/QrCodeGenerator
+ * Copyright (c) Project Nayuki (MIT License)
+ * https://www.nayuki.io/page/qr-code-generator-library
+ * Downgrade to C# 2.0 by ArdeshirV
+ */
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("QrCode")]
+[assembly: AssemblyDescription ("QR Code Generator for .Net\n" +
+                                "Copyright (c) ArdeshirV@protonmail.com (MIT License)\n" +
+                                "https://ardeshirv.github.io/ArdeshirV.Utility.QrCode/\n" +
+                                "Downgrade to C# 2.0 by ArdeshirV\n" +
+                                "Copyright (c) Manuel Bleichenbacher (MIT License)\n" +
+                                "https://github.com/manuelbl/QrCodeGenerator\n" +
+                                "Convert to C# by Manuel Bleichenbacher\n" +
+                                "Copyright (c) Project Nayuki (MIT License)\n" +
+                                "https://www.nayuki.io/page/qr-code-generator-library")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("ArdeshirV")]
+[assembly: AssemblyProduct ("QrCode")]
+[assembly: AssemblyCopyright ("Copyright Manuel Bleichenbacher & Project Nayuki & ArdeshirV (MIT License)")]
+[assembly: AssemblyTrademark ("QrCode")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.4.1")]
+[assembly: Guid ("24d77e3b-9f98-4fec-be75-1ae771838c91")]
